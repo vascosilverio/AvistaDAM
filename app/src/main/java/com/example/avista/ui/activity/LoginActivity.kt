492ffd9f2bfd14f58f7a78e101405e1951f22b3f
Rúben Xavier Oliveira Garcia <ruben.xaviergarcia93@gmail.com>
2023-11-18 14:23:43 +0000

    Verificações de autenticação

--- app/src/main/java/com/example/avista/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/avista/ui/activity/LoginActivity.kt
@@ -33,6 +33,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 const val BASE_URL = "https://api.sheety.co/6c966be3aa95146fdfd60b287a41e909/aveDam/"
+var userAuth = ""
 
 class LoginActivity : AppCompatActivity() {
 
@@ -44,15 +45,16 @@ class LoginActivity : AppCompatActivity() {
         val loginButton2: Button = findViewById(R.id.loginButton2)
 
         loginButton2.setOnClickListener {
+            userAuth = ""
             // Retrieve username/email and password from EditText fields
             val username = findViewById<EditText>(R.id.editTextUsername).text.toString()
             val password = findViewById<EditText>(R.id.editTextPassword).text.toString()
             // Toast.makeText(this, "Users: " + user, Toast.LENGTH_SHORT).show()
-            getUsers();
+            getUsers(username, password);
         }
 
     }
-     private fun getUsers(){
+     private fun getUsers(username: String, password: String){
          val retrofitBuilder = Retrofit.Builder()
              .addConverterFactory(GsonConverterFactory.create())
              .baseUrl(BASE_URL)
@@ -69,10 +71,19 @@ class LoginActivity : AppCompatActivity() {
                      responseBody?.let {
                          val myStringBuilder = StringBuilder()
                          for (dados in it.utilizadorList) {
+                             if(username == dados.userId){
+                                 if(password == dados.password) {
+                                     userAuth = username
+                                 }
+                             }
                              myStringBuilder.append(dados.userId)
-                             myStringBuilder.append("\n")
+                             //myStringBuilder.append("\n")
                          }
-                         findViewById<TextView>(R.id.printTest2).text = myStringBuilder.toString()
+                         if(userAuth == ""){
+                             findViewById<TextView>(R.id.printTest).text = "Verifique o nome de utilizador ou a password."
+                         } else {
+                            findViewById<TextView>(R.id.printTest).text = "User autenticado: " + userAuth
+                         }
                      }
                  } else {
                      Log.d("LoginActivity", "onResponse: ${response.code()}")
