1ee0174bfa1a5d7ffecf1859aa52c80f6c93e505
Rúben Xavier Oliveira Garcia <ruben.xaviergarcia93@gmail.com>
2023-12-21 12:41:17 +0000

    Dialog para mostrar os detalhes da observação.

--- app/src/main/java/com/example/avista/adapter/ObservacaoListAdapter.kt
+++ app/src/main/java/com/example/avista/adapter/ObservacaoListAdapter.kt
@@ -1,7 +1,10 @@
 package com.example.avista.adapter;
 
 
+import android.app.Dialog
 import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.util.Base64
 import android.util.Log
 import android.view.LayoutInflater
@@ -14,12 +17,18 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.avista.R
 import com.example.avista.model.Observacao
 import com.squareup.picasso.Picasso
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
 
 class ObservacaoListAdapter(val listaObservacoes: ArrayList<Observacao>, val onClickListener: OnClickListener) :
     RecyclerView.Adapter<ObservacaoListAdapter.ObservacaoViewHolder>() {
 
     class ObservacaoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val textView: TextView = itemView.findViewById(R.id.card_Data_Observacao)
+        val txtDescricao: TextView = itemView.findViewById(R.id.txtDescricao)
+        val txtEspecie: TextView = itemView.findViewById(R.id.txtEspecie)
         val image: ImageView = itemView.findViewById(R.id.card_thumbnail_Observacao)
         val btnDetalhes: Button = itemView.findViewById(R.id.btnAbreDialog)
 
@@ -43,15 +52,46 @@ class ObservacaoListAdapter(val listaObservacoes: ArrayList<Observacao>, val onC
     override fun onBindViewHolder(holder: ObservacaoViewHolder, position: Int) {
         val observacao = listaObservacoes[position]
         holder.textView.setText(observacao.data)
+        holder.txtEspecie.setText(observacao.especie)
+        holder.txtDescricao.setText(observacao.descricao)
         Picasso.get().load(observacao.foto).into(holder.image)
 
-        /*holder.itemView.setOnClickListener{
-            Log.d("Entrou","yeah")
-            onClickListener.onClick(observacao)
-        }*/
-
         holder.btnDetalhes.setOnClickListener{
-            Log.d("Clique",observacao.especie.toString())
+
+            // Ao abrir os detalhes de uma observação, construir uma Dialog com recurso ao detalhes_observacao.xml
+            val detalhes = Dialog(holder.itemView.context)
+            detalhes.setContentView(R.layout.detalhes_observacao)
+            detalhes.window?.setBackgroundDrawable(ColorDrawable(Color.WHITE))
+            // colocar a dialog com 90% do tamanho horizontal
+            val largura = (holder.itemView.context.resources.displayMetrics.widthPixels * 0.90).toInt()
+            detalhes.window?.setLayout(largura, ViewGroup.LayoutParams.WRAP_CONTENT)
+            detalhes.setCancelable(true)
+
+            // construir o mapa
+            val mapView = detalhes.findViewById<MapView>(R.id.mapa)
+            mapView.setTileSource(TileSourceFactory.MAPNIK)
+            mapView.setMultiTouchControls(true)
+
+            val marcador = Marker(mapView)
+            marcador.position = GeoPoint(observacao.lat!!.toDouble(), observacao.long!!.toDouble())
+            mapView.overlays.add(marcador)
+            mapView.controller.setZoom(12.0)
+            mapView.controller.setCenter(GeoPoint(observacao.lat!!.toDouble(), observacao.long!!.toDouble() ))
+
+            val txtdDescricao = detalhes.findViewById<TextView>(R.id.txtDescricao)
+            val txtEspecie = detalhes.findViewById<TextView>(R.id.txtEspecie)
+            val txtdData = detalhes.findViewById<TextView>(R.id.txtData)
+            val aveImg = detalhes.findViewById<ImageView>(R.id.imgAve)
+            val fechar = detalhes.findViewById<Button>(R.id.btnFecharDialog)
+            Picasso.get().load(observacao.foto).into(aveImg)
+            txtEspecie.text = observacao.especie
+            txtdDescricao.text = observacao.descricao
+            txtdData.text = observacao.data
+            detalhes.show()
+
+            fechar.setOnClickListener{
+                detalhes.dismiss()
+            }
         }
     }
 }
