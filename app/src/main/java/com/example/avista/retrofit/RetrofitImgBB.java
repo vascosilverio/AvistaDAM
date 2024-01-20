28164e1a12233ba879896cb84cbe898d708a5446
rubeng07 <ruben.xaviergarcia93@gmail.com>
2024-01-20 17:38:08 +0000

    Ajuste do login para funcionar em landscape

--- app/src/main/java/com/example/avista/retrofit/RetrofitImgBB.java
+++ app/src/main/java/com/example/avista/retrofit/RetrofitImgBB.java
@@ -2,6 +2,8 @@ package com.example.avista.retrofit;
 
 import android.content.Context;
 
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -19,7 +21,13 @@ public class RetrofitImgBB {
                     .build();
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .client(okHttpClient)
+                    .client(
+                            new OkHttpClient.Builder()
+                                    .connectTimeout(30, TimeUnit.SECONDS)
+                                    .readTimeout(30, TimeUnit.SECONDS)
+                                    .writeTimeout(30, TimeUnit.SECONDS)
+                                    .build()
+                    )
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
